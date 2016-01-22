27be1e8653f40456cb3ee58331edcb01e56c56d3
App Academy Student <appacademy@Apps-Mac-mini-102.local>
2016-01-22 11:24:54 -0500

    Create model and controller for Users, continue setting up file structure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bench.destroy_all
+
+bench1_description = "A nice bench near greenery on Hagiwara Tea Garden Drive."
+bench1_lat = 37.771476
+bench1_lng = -122.467664
+bench1 = Bench.create({
+  description: bench1_description,
+  lat: bench1_lat,
+  lng: bench1_lng
+})
+
+bench2_description = "An understated bench near Music Concourse Drive."
+bench2_lat = 37.770152
+bench2_lng = -122.467105
+bench2 = Bench.create({
+  description: bench2_description,
+  lat: bench2_lat,
+  lng: bench2_lng
+})
